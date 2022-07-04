18db58150d9df4abe569308585b7a5060ef8ad29
Gabriel Vinhas <105182274+GabrielVinhas@users.noreply.github.com>
2022-07-04 16:06:31 -0300

    Add files via upload

---
+++ js/favorites.js
@@ -0,0 +1,139 @@
+import { GithubUser } from "../GithubUser.js"
+
+
+
+export class Favorites {
+  constructor(root) {
+    this.root = document.querySelector(root)
+    this.load()
+  }
+
+  load() {
+    this.entries= JSON.parse(localStorage.getItem('@github-favorites:')) || []
+  }
+
+  save() {
+    localStorage.setItem('@github-favorites:', JSON.stringify(this.entries))
+  }
+
+  async add(username) {
+    try {
+
+      const userExist = this.entries.find(entry => entry.login === username)
+
+      if(userExist) {
+        throw new Error('Usuário já cadastrado!')
+      } 
+
+      const user = await GithubUser.search(username) 
+      
+      if(user.login === undefined) {
+        throw new Error('Ixi, usuário não encontrado!😰')
+      }
+
+      this.entries = [user, ...this.entries]
+      this.update()
+      this.save()
+
+    } catch(error) {
+      alert(error.message)
+    }
+  }
+
+  delete(user) {
+    const filteredEntries = this.entries
+    .filter(entry => entry.login !== user.login)
+
+  this.entries = filteredEntries
+  this.update()
+  this.save()
+  }
+}
+
+
+export class FavoritesView extends Favorites {
+  constructor(root) {
+    super(root)
+
+    this.tbody = this.root.querySelector('table tbody')
+
+    this.update()
+    this.onadd()
+  }
+
+  onadd() {
+    const addButton = this.root.querySelector('.search button')
+    addButton.onclick = () => {
+      const { value } = this.root.querySelector('.search input')
+
+      this.add(value)
+    } 
+  }
+
+  Nenhum() {
+    this.entries.length === 0 ?
+    document.querySelector('.nenhum').classList.remove('sr-only') :
+    document.querySelector('.nenhum').classList.add('sr-only')
+  }
+
+  update() {
+    this.Nenhum()
+    this.removeAllTr()
+
+    this.entries.forEach( user => {
+      const row = this.createRow()
+
+      row.querySelector('.user img').src = `https://github.com/${user.login}.png`
+      row.querySelector('.user img').alt = `Imagem de ${user.name}`
+      row.querySelector('.user a').href = `https://github.com/${user.login}`
+      row.querySelector('.user p').textContent = user.name
+      row.querySelector('.user span').textContent = user.login
+      row.querySelector('.repositories').textContent = user.public_repos
+      row.querySelector('.followers').textContent = user.followers
+
+      row.querySelector('.remove').onclick = () => {
+        const isOk = confirm('Tem certeza que deseja deletar essa linha?')
+        if(isOk) {
+          this.delete(user)
+        }
+      }
+
+      this.tbody.append(row)
+    })
+
+    
+  }
+
+  createRow() {
+    const tr = document.createElement('tr')
+
+    tr.innerHTML = `
+    
+      <td class="user">
+        <img
+          src="https://github.com/Caarlos7x.png"
+          alt="Imagem do Carlos"
+        />
+        <a href="https://github.com/Caarlos7x" target="_blank">
+          <p>Carlos Augusto</p>
+          <span>Caarlos7x</span>
+        </a>
+      </td>
+
+      <td class="repositories">21</td>
+      <td class="followers">7</td>
+      <td>
+        <button id="remover" class="remove">Remove</button>
+        </td>
+    `
+
+    return tr 
+  }
+
+  removeAllTr() {
+    this.tbody.querySelectorAll('tr')
+    .forEach((tr) => {
+      tr.remove()
+    })
+  }
+}
